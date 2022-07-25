75b307df7916370f40689d67aa64fbba3f3e5902
promise <proncotech@gmail.com>
2022-07-25 02:17:58 +0200

    new file

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagram
+export POSTGRES_PASSWORD=udagram123
+export POSTGRES_HOST=udagram-db.copj9zki1gzd.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=proncodoe12345679-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
